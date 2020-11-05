f144dcef9c57992fbb1a177fc3352166465d7b48
FirtiCe <71794701+FirtiCe@users.noreply.github.com>
2020-11-05 15:52:46 +0300

    Update app.module.ts

--- test2/src/app/app.module.ts
+++ test2/src/app/app.module.ts
@@ -1,19 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-    imports: [
-        BrowserModule,
-        ReactiveFormsModule,
-        FormsModule
-    ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
